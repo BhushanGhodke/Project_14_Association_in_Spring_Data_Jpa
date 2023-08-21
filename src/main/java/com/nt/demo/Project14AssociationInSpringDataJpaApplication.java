9b5ebf4d49883ce5e68178b851ba6c989e71afbd
BhushanGhodke <bhushanghodke1500@gmail.com>
2023-08-21 10:21:58 +0530

    First commit

---
+++ src/main/java/com/nt/demo/Project14AssociationInSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.nt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project14AssociationInSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project14AssociationInSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nt/demo/model/Custommer.java
@@ -0,0 +1,8 @@
+package com.nt.demo.model;
+
+import java.util.List;
+
+public class Custommer {
+
+	
+}
